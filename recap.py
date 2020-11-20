2597cd2dc40d1dee5e3a91c009078c1d92f6f46f
hannahgreen1 <hannahgreen94@icloud.com>
2020-11-20 14:44:03 +0000

    test repo

---
+++ recap.py
@@ -0,0 +1,18 @@
+avengers = {
+  "ironman": {
+    "name": "Tony Stark",
+    "moves": {
+      "punch": 10,
+      "kick": 100
+    }
+  },
+  "hulk": {
+    "name": "Bruce Banner",
+    "moves": {
+      "smash": 1000,
+      "roll": 500
+    }
+  }
+}
+print(avengers["ironman"]["moves"]["punch"])
+print(avengers["hulk"]["name"])
